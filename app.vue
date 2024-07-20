14fd60266bd557dcd79de9c8e6bc790812c9ea92
Akayy <73772701+TechAkayy@users.noreply.github.com>
2024-07-21 02:11:42 +1000

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <NuxtImg
-      src="https://images.unsplash.com/photo-1476297816471-97713c4d237c?crop=entropy&cs=srgb&fm=jpg&ixid=M3wyMDkyMnwwfDF8c2VhcmNofDF8fHRyZWVzfGVufDB8fHx8MTcyMTQ3MjkyOXww&ixlib=rb-4.0.3&q=85"></NuxtImg>
+      src="https://images.unsplash.com/photo-1476297816471-97713c4d237c?crop=entropy&cs=srgb&fm=jpg"></NuxtImg>
     <NuxtImg
-      src="/unsplash/photo-1476297816471-97713c4d237c?crop=entropy&cs=srgb&fm=jpg&ixid=M3wyMDkyMnwwfDF8c2VhcmNofDF8fHRyZWVzfGVufDB8fHx8MTcyMTQ3MjkyOXww&ixlib=rb-4.0.3&q=85"></NuxtImg>
+      src="/unsplash/photo-1476297816471-97713c4d237c?crop=entropy&cs=srgb&fm=jpg"></NuxtImg>
   </div>
 </template>
